a84f9478156b353c07465b5dda89989fdbc8ef10
Carlos Tapia <ctapiadev@gmail.com>
2026-02-03 20:31:50 -0300

    corrutinas

--- app/src/main/java/com/example/taskmaster/controller/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/controller/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.taskmaster.controller
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.taskmaster.R
 import com.example.taskmaster.databinding.ActivityMainBinding
 import com.example.taskmaster.model.TareaRepository
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +24,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupListener()
+        // setupListener()
+        binding.btnAgregar.setOnClickListener {
+            val texto = binding.etTarea.text.toString();
+            // guardarTareaProcesando(texto)
+            guardarConManejoDeErrores(texto)
+        }
+
+        binding.btnLimpiar.setOnClickListener {
+            repositorio.limpiarTodo()
+            actualizarInterfaz()
+        }
     }
 
-    private fun setupListener() {
+    /* private fun setupListener() {
         binding.btnAgregar.setOnClickListener {
             val texto = binding.etTarea.text.toString()
             repositorio.agregarTarea(texto)
@@ -33,6 +48,48 @@ class MainActivity : AppCompatActivity() {
         binding.btnLimpiar.setOnClickListener {
             repositorio.limpiarTodo()
             actualizarInterfaz()
+        }
+    } */
+
+    private fun guardarTareaProcesando(descripcion: String) {
+        // Corrutina
+        lifecycleScope.launch {
+            binding.progressBar.visibility = View.VISIBLE
+            binding.btnAgregar.isEnabled = false
+            binding.etTarea.isEnabled = false
+
+            val exito = repositorio.agregarTareaConRetraso(descripcion)
+
+            binding.progressBar.visibility = View.GONE
+            binding.btnAgregar.isEnabled = true
+            binding.etTarea.isEnabled = true
+
+            if (exito) {
+                binding.etTarea.text.clear()
+                actualizarInterfaz()
+            } else {
+                Toast.makeText(this@MainActivity, "Error: Tarea vacía", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun guardarConManejoDeErrores(texto: String) {
+        lifecycleScope.launch {
+            binding.progressBar.visibility = View.VISIBLE
+            binding.tvEstado.text = "Conectando con el servidor..."
+
+            val resultado = repositorio.agregarTareaEnNube(texto)
+
+            binding.progressBar.visibility = View.GONE
+
+            resultado.onSuccess {
+                binding.etTarea.text.clear()
+                binding.tvEstado.text = "Guardado con éxito"
+                actualizarInterfaz()
+            }.onFailure { error ->
+                binding.tvEstado.text = "Falló: ${error.message}"
+                binding.tvEstado.setTextColor(Color.RED)
+            }
         }
     }
 
--- app/src/main/java/com/example/taskmaster/model/TareaRepository.kt
+++ app/src/main/java/com/example/taskmaster/model/TareaRepository.kt
@@ -1,11 +1,42 @@
 package com.example.taskmaster.model
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
 class TareaRepository {
     private val listaTareas = mutableListOf<Tarea>();
 
-    fun agregarTarea(descripcion: String) {
+    /* fun agregarTarea(descripcion: String) {
         if (descripcion.isNotBlank()) {
             listaTareas.add(Tarea(descripcion))
+        }
+    } */
+
+    suspend fun agregarTareaConRetraso(descripcion: String): Boolean {
+        return withContext(Dispatchers.IO) {
+            delay(3000)
+            if (descripcion.isNotBlank()) {
+                listaTareas.add(Tarea(descripcion))
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    suspend fun agregarTareaEnNube(descripcion: String): Result<Boolean> {
+        return withContext(Dispatchers.IO) {
+            delay(3000)
+
+            val errorAleatorio = (1..3).random()
+
+            if (errorAleatorio == 1) {
+                Result.failure(Exception("Error de conexión: El servidor no responde"))
+            } else {
+                listaTareas.add(Tarea(descripcion))
+                Result.success(true)
+            }
         }
     }
 
